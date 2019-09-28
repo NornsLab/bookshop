2aa1b51541b734c14e37b14d327c4a1e58851485
liwei <liweityut@163.com>
2019-09-28 15:44:33 +0800

    Initial commit

---
+++ cloud-discovery/src/main/java/io/github/nornslab/bookshop/clouddiscovery/CloudDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package io.github.nornslab.bookshop.clouddiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudDiscoveryApplication.class, args);
+	}
+
+}
